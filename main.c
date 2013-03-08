7f33c7e9a0f492b7de70e982ee05e00a52295754
gregory144 <greg@gtgross.com>
2013-03-09 00:39:06 +1100

    Added export option to print export/setenv command

--- main.c
+++ main.c
@@ -9,6 +9,13 @@
 #include "file.h"
 #include "match.h"
 
+#define ENV_VAR_NAME "PATH"
+
+typedef enum { print_default, print_export, print_list, print_search, print_quiet } print_mode_t;
+
+typedef enum { export_default, export_setenv } export_mode_t;
+
+
 // cached copy of all files in the path
 node_t* directory_names = NULL;
 file_list_t* all_files = NULL;
@@ -35,8 +42,8 @@ void free_all_files() {
 
 int main(int argc, char **argv) {
   int c;
-  bool print = true;
-  bool list = false;
+  print_mode_t print_mode = print_default;
+  export_mode_t export_mode = export_default;
   node_t* directoriesToAdd = NULL;
   node_t* directoriesToRemove = NULL;
   node_t* filesToSearch = NULL;
@@ -48,6 +55,7 @@ int main(int argc, char **argv) {
       {"add",     required_argument, 0, 'a'},
       {"rm",      required_argument, 0, 'r'},
       {"search",  required_argument, 0, 's'},
+      {"export",  optional_argument, 0, 'e'},
       {"list",    no_argument,       0, 'l'},
       {"verbose", no_argument,       0, 'v'},
       {"warn",    no_argument,       0, 'w'},
@@ -55,7 +63,7 @@ int main(int argc, char **argv) {
       {"help",    no_argument,       0, 'h'}
     };
 
-    c = getopt_long(argc, argv, "-a:r:s:lvwqh?", long_options, &option_index);
+    c = getopt_long(argc, argv, "-a:r:s:elvwqh?", long_options, &option_index);
     if (c == -1)
       break;
 
@@ -78,8 +86,7 @@ int main(int argc, char **argv) {
         break;
 
       case 's':
-        // turn off print
-        print = false;
+        print_mode = print_search;
 
         tmp = malloc(sizeof(node_t));
         tmp->val = strdup(optarg);
@@ -87,9 +94,19 @@ int main(int argc, char **argv) {
         filesToSearch = tmp;
         break;
 
+      case 'e':
+        print_mode = print_export;
+        if (optarg) {
+          if (strcmp(optarg, "setenv") == 0) {
+            export_mode = export_setenv;
+          } else {
+            export_mode = export_default;
+          }
+        }
+        break;
+
       case 'l':
-        list = true;
-        print = false;
+        print_mode = print_list;
         break;
 
       case 'v':
@@ -102,7 +119,7 @@ int main(int argc, char **argv) {
         break;
 
       case 'q':
-        print = false;
+        print_mode = print_quiet;
         break;
 
       default:
@@ -116,15 +133,15 @@ int main(int argc, char **argv) {
     }
   }
 
-  char* orig_path = getenv("PATH");
+  char* orig_path = getenv(ENV_VAR_NAME);
   int path_len = 0;
   char* path_s = "";
   if (orig_path != NULL) {
     path_len = strlen(orig_path);
     path_s = strndup(orig_path, path_len);
-    print_verbose("PATH=%s\n", path_s);
+    print_verbose("%s=%s\n", ENV_VAR_NAME, path_s);
   } else {
-    print_verbose("Unable to read PATH environment variable.\n");
+    print_verbose("Unable to read %s environment variable.\n", ENV_VAR_NAME);
   }
 
   path_t* path = path_parse(path_s);
@@ -144,53 +161,72 @@ int main(int argc, char **argv) {
 
   path_clean(path);
 
-  if (filesToSearch) {
-    file_list_t* files = get_all_files(path);
-    for (entry = filesToSearch; entry; entry = entry->next) {
-      print_verbose("Searching for: `%s`\n", entry->val);
-      char* searchResult = path_search(path, entry->val);
-      if (searchResult) {
-        printf("exact: %s\n", searchResult);
-      } else {
-        print_warning("`%s` not found in path.\n", entry->val);
-        match_t* matches = find_best_matches(entry->val, files);
-        match_t* currMatch;
-        int i;
-        for (i = 0; i < 10; i++) {
-          currMatch = &matches[i];
-          if (currMatch && currMatch->metric > 0.4) {
-            printf("almost: %s", currMatch->file->full_path);
-            if (!currMatch->file->executable) {
-              printf(" (not executable)");
-            }
-            printf("\n");
-          }
-        }
-        free(matches);
-      }
-      free(searchResult);
-    }
-  }
-
   if (warnings_are_on()) {
     path_warnings(path);
   }
 
-  if (list) {
-    file_list_t* files = get_all_files(path);
-    file_list_t* entry = NULL;
-
-    for (entry = files; entry; entry = entry->next) {
-      if (entry->executable) {
-        printf("%s\n", entry->full_path);
+  switch (print_mode) {
+    case print_search:
+      if (filesToSearch) {
+        file_list_t* files = get_all_files(path);
+        for (entry = filesToSearch; entry; entry = entry->next) {
+          print_verbose("Searching for: `%s`\n", entry->val);
+          char* searchResult = path_search(path, entry->val);
+          if (searchResult) {
+            printf("exact: %s\n", searchResult);
+          } else {
+            print_warning("`%s` not found in path.\n", entry->val);
+            match_t* matches = find_best_matches(entry->val, files);
+            match_t* currMatch;
+            int i;
+            for (i = 0; i < 10; i++) {
+              currMatch = &matches[i];
+              if (currMatch && currMatch->metric > 0.4) {
+                printf("almost: %s", currMatch->file->full_path);
+                if (!currMatch->file->executable) {
+                  printf(" (not executable)");
+                }
+                printf("\n");
+              }
+            }
+            free(matches);
+          }
+          free(searchResult);
+        }
       }
-    }
-  } else {
-    if (print) {
-      char* processed_path = path_to_string(path);
-      printf("%s\n", processed_path);
-      free(processed_path);
-    }
+      break;
+    case print_export: {
+        char* processed_path = path_to_string(path);
+        char* cmd_name = "export";
+        char* key_value_separator = "=";
+        if (export_mode == export_setenv) {
+          cmd_name = "setenv";
+          key_value_separator = " ";
+        }
+        printf("%s %s%s%s\n", cmd_name, ENV_VAR_NAME, key_value_separator, processed_path);
+        free(processed_path);
+      }
+      break;
+    case print_list: {
+        file_list_t* files = get_all_files(path);
+        file_list_t* entry = NULL;
+
+        for (entry = files; entry; entry = entry->next) {
+          if (entry->executable) {
+            printf("%s\n", entry->full_path);
+          }
+        }
+      }
+      break;
+    case print_quiet:
+      break;
+    case print_default:
+    default: {
+        char* processed_path = path_to_string(path);
+        printf("%s\n", processed_path);
+        free(processed_path);
+      }
+      break;
   }
 
   free_all_files();
