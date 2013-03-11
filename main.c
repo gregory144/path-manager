9d538eb19578e2f1958fdf975fca5c1fad5c93ad
gregory144 <greg@gtgross.com>
2013-03-12 00:02:19 +1100

    Only save path if it has been modified

--- main.c
+++ main.c
@@ -43,9 +43,10 @@ int main(int argc, char **argv) {
   int c;
   print_mode_t print_mode = print_path;
   export_mode_t export_mode = export_default;
-  bool save = true;
   bool install = false;
   bool install_global = false;
+  bool modified = false;
+  bool failed = false;
 
   node_t* dirs_to_add = NULL;
   node_t* dirs_to_remove = NULL;
@@ -61,7 +62,6 @@ int main(int argc, char **argv) {
       {"export",  optional_argument, 0, 'e'},
       {"install", no_argument,       0, 'i'},
       {"install-global", no_argument,0, 'I'},
-      {"no-save", no_argument,       0, 'n'},
       {"list",    no_argument,       0, 'l'},
       {"verbose", no_argument,       0, 'v'},
       {"warn",    no_argument,       0, 'w'},
@@ -69,7 +69,7 @@ int main(int argc, char **argv) {
       {"help",    no_argument,       0, 'h'}
     };
 
-    c = getopt_long(argc, argv, "-a:r:s:eiInlvwqh?", long_options, &option_index);
+    c = getopt_long(argc, argv, "-a:r:s:eiIlvwqh?", long_options, &option_index);
     if (c == -1)
       break;
 
@@ -77,8 +77,10 @@ int main(int argc, char **argv) {
 
       case 'a':
         if (optarg[0] != '/') {
-          fprintf(stderr, "Could not add %s to %s, it must be an absolute path.", optarg, ENV_VAR_NAME);
+          fprintf(stderr, "Could not add %s to %s, it must be an absolute path.\n", optarg, ENV_VAR_NAME);
+          failed = true;
         } else {
+          modified = true;
           tmp = malloc(sizeof(node_t));
           tmp->val = strdup(optarg);
           print_verbose("Adding: %s\n", tmp->val);
@@ -89,8 +91,10 @@ int main(int argc, char **argv) {
 
       case 'r':
         if (optarg[0] != '/') {
-          fprintf(stderr, "Could not remove %s from %s, it must be an absolute path.", optarg, ENV_VAR_NAME);
+          fprintf(stderr, "Could not remove %s from %s, it must be an absolute path.\n", optarg, ENV_VAR_NAME);
+          failed = true;
         } else {
+          modified = true;
           tmp = malloc(sizeof(node_t));
           tmp->val = strdup(optarg);
           print_verbose("Removing: %s\n", tmp->val);
@@ -127,10 +131,6 @@ int main(int argc, char **argv) {
         install = true;
         break;
 
-      case 'n':
-        save = false;
-        break;
-
       case 'l':
         print_mode = print_list;
         break;
@@ -151,113 +151,124 @@ int main(int argc, char **argv) {
       default:
       case 'h':
       case '?':
-        free_nodes(dirs_to_add);
-        free_nodes(dirs_to_remove);
-        free_nodes(files_to_search);
         display_usage();
+        failed = true;
         break;
     }
   }
 
-  if (install || install_global) {
-    install_in_shell(install_global);
-  }
+  if (!failed) {
 
-  path_t* path = path_load(ENV_VAR_NAME);
-
-  node_t* entry;
-
-  for (entry = dirs_to_add; entry; entry = entry->next) {
-    int added = path_add(path, entry->val);
-    if (!added) {
-      print_warning("Directory `%s` not added to the path due to warnings. Use --force to override.\n", entry->val);
+    if (install || install_global) {
+      install_in_shell(install_global);
     }
-  }
 
-  for (entry = dirs_to_remove; entry; entry = entry->next) {
-    print_verbose("Removing `%s` from path.\n", entry->val);
-    path_rm(path, entry->val);
-  }
+    path_t* path = path_load(ENV_VAR_NAME);
 
-  path_clean(path);
+    node_t* entry;
 
-  if (warnings_are_on()) {
-    path_warnings(path);
-  }
+    for (entry = dirs_to_add; entry; entry = entry->next) {
+      int added = path_add(path, entry->val);
+      if (!added) {
+        print_warning("Directory `%s` not added to the path due to warnings. Use --force to override.\n", entry->val);
+      }
+    }
 
-  switch (print_mode) {
-    case print_search:
-      if (files_to_search) {
-        file_list_t* files = get_all_files(path);
-        for (entry = files_to_search; entry; entry = entry->next) {
-          print_verbose("Searching for: `%s`\n", entry->val);
-          char* search_result = path_search(path, entry->val);
-          if (search_result) {
-            printf("exact: %s\n", search_result);
-          } else {
-            print_warning("`%s` not found in path.\n", entry->val);
-            match_t* matches = find_best_matches(entry->val, files);
-            match_t* curr_match;
-            int i;
-            for (i = 0; i < 10; i++) {
-              curr_match = &matches[i];
-              if (curr_match && curr_match->metric > 0.4) {
-                printf("almost: %s", curr_match->file->full_path);
-                if (!curr_match->file->executable) {
-                  printf(" (not executable)");
+    for (entry = dirs_to_remove; entry; entry = entry->next) {
+      print_verbose("Removing `%s` from path.\n", entry->val);
+      path_rm(path, entry->val);
+    }
+
+    path_clean(path);
+
+    if (warnings_are_on()) {
+      path_warnings(path);
+    }
+
+    switch (print_mode) {
+      case print_search:
+        if (files_to_search) {
+          file_list_t* files = get_all_files(path);
+          for (entry = files_to_search; entry; entry = entry->next) {
+            print_verbose("Searching for: `%s`\n", entry->val);
+            char* search_result = path_search(path, entry->val);
+            if (search_result) {
+              printf("exact: %s\n", search_result);
+            } else {
+              print_warning("`%s` not found in path.\n", entry->val);
+              match_t* matches = find_best_matches(entry->val, files);
+              match_t* curr_match;
+              int i;
+              for (i = 0; i < 10; i++) {
+                curr_match = &matches[i];
+                if (curr_match && curr_match->metric > 0.4) {
+                  printf("almost: %s", curr_match->file->full_path);
+                  if (!curr_match->file->executable) {
+                    printf(" (not executable)");
+                  }
+                  printf("\n");
                 }
-                printf("\n");
               }
+              free(matches);
             }
-            free(matches);
-          }
-          free(search_result);
-        }
-      }
-      break;
-    case print_export: {
-        char* processed_path = path_to_string(path);
-        char* cmd_name = "export";
-        char* key_value_separator = "=";
-        if (export_mode == export_setenv) {
-          cmd_name = "setenv";
-          key_value_separator = " ";
-        }
-        printf("%s %s%s%s\n", cmd_name, ENV_VAR_NAME, key_value_separator, processed_path);
-        free(processed_path);
-      }
-      break;
-    case print_list: {
-        file_list_t* files = get_all_files(path);
-        file_list_t* entry = NULL;
-
-        for (entry = files; entry; entry = entry->next) {
-          if (entry->executable) {
-            printf("%s\n", entry->full_path);
+            free(search_result);
           }
         }
-      }
-      break;
-    case print_quiet:
-      break;
-    case print_path:
-    default: {
-        char* processed_path = path_to_string(path);
-        printf("%s\n", processed_path);
-        free(processed_path);
-      }
-      break;
-  }
+        break;
+      case print_export: {
+          char* processed_path = path_to_string(path);
+          char* cmd_name = "export";
+          char* key_value_separator = "=";
+          if (export_mode == export_setenv) {
+            cmd_name = "setenv";
+            key_value_separator = " ";
+          }
+          printf("%s %s%s%s\n", cmd_name, ENV_VAR_NAME, key_value_separator, processed_path);
+          free(processed_path);
+        }
+        break;
+      case print_list: {
+          file_list_t* files = get_all_files(path);
+          file_list_t* entry = NULL;
 
-  if (save) {
-    path_save(path);
+          for (entry = files; entry; entry = entry->next) {
+            if (entry->executable) {
+              printf("%s\n", entry->full_path);
+            }
+          }
+        }
+        break;
+      case print_quiet:
+        break;
+      case print_path:
+      default: {
+          path_entry_t* entry;
+
+          for (entry = path->head; entry; entry = entry->next) {
+            printf("%s\n", entry->directory);
+          }
+        }
+        break;
+    }
+
+    if (modified) {
+      path_save(path);
+    }
+
+    path_free(path);
   }
 
   free_all_files();
   free_nodes(dirs_to_add);
   free_nodes(dirs_to_remove);
   free_nodes(files_to_search);
-  path_free(path);
+
+  if (failed) {
+    free_nodes(dirs_to_add);
+    free_nodes(dirs_to_remove);
+    free_nodes(files_to_search);
+    exit(EXIT_FAILURE);
+  }
   return 0;
 }
 
